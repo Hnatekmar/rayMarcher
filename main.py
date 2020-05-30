797daa0d431e668ab69b5ec54736155f49c43004
Martin Hnátek <martinhnatek@sanezoo.com>
2020-05-30 12:31:58 +0200

    Screen

--- main.py
+++ main.py
@@ -27,10 +27,13 @@ class SphereGroup(ObjectGroup):
 def march(origins: torch.Tensor, directions: torch.Tensor, transform: torch.Tensor, objects: List[ObjectGroup],
           epsilon=1e-2, maximum_iterations=100, debug=False):
     transformed_rays = origins @ transform
+    transformed_rays.cuda()
     rows = transformed_rays.shape[0]
     distances = torch.ones(rows, 1) * float('inf')
+    distances.cuda()
     mask = torch.zeros(rows, 1).bool()
-    points = transformed_rays.tolist()
+    if debug:
+        points = transformed_rays.tolist()
     for _ in range(maximum_iterations):
         for group in objects:
             current_distances = group.nearest(transformed_rays).reshape((rows, 1))
@@ -38,35 +41,38 @@ def march(origins: torch.Tensor, directions: torch.Tensor, transform: torch.Tens
             mask[distances < epsilon] = True
             distances[mask] = 0
             transformed_rays[:, :3] += directions * distances
-            points += transformed_rays.tolist()
+            if debug:
+                points += transformed_rays.tolist()
             if mask.all():
                 break
-    if debug:
-        points_ = np.array(points)
-        vis = open3d.geometry.PointCloud(
-            points=open3d.utility.Vector3dVector(points_[:, :3])
-        )
-        open3d.visualization.draw_geometries([vis])
+        if debug:
+            points_ = np.array(points)
+            vis = open3d.geometry.PointCloud(
+                points=open3d.utility.Vector3dVector(points_[:, :3])
+            )
+            open3d.visualization.draw_geometries([vis])
     infinity = torch.ones(rows, 1)
     infinity[~mask] = float('inf')
-    transformed_rays *= infinity
+    transformed_rays += infinity
     return transformed_rays
 
 
 def main():
     sphere_group = SphereGroup()
-    sphere_group.add_sphere([0, 0, 0], 5)
+    sphere_group.add_sphere([0, 0, 500], 200)
     torch.cuda.set_device(0)
-    import math
-    x = torch.Tensor([
-       [math.sin(math.degrees(i)) * 50, math.cos(math.degrees(i)) * 50, 0, 1] for i in range(0, 360, 10)
+    rays = torch.Tensor([
+        [x, y, 0, 1]
+        for x in range(0, 800)
+        for y in range(0, 600)
     ])
-    res = march(x, -x[:, :3]/50.0, torch.eye(4, 4), [sphere_group], debug=True)
+
+    directions = torch.zeros_like(rays)
+    directions = directions[:, :3]
+    directions[:, 2] = 1
+
+    res = march(rays, directions, torch.eye(4, 4), [sphere_group], debug=False, maximum_iterations=50)
     res = res[:, :2]
-    import matplotlib.pyplot as plt
-    plt.plot(res[:, 0], res[:, 1], 'r.')
-    plt.plot(x[:, 0], -x[:, 1], 'b.')
-    plt.show()
 
 
 if __name__ == '__main__':
