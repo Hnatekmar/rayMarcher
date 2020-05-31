11d4881dbd8fff4b8d7d6bd49312e034a3910d72
Martin Hnátek <martinhnatek@sanezoo.com>
2020-05-31 10:16:25 +0200

    Cuda only implementation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import open3d
 import torch
 from typing import List
 
+import tqdm
+
 
 class ObjectGroup:
     def nearest(self, points: torch.Tensor) -> torch.Tensor:
@@ -22,10 +24,11 @@ class SphereGroup(ObjectGroup):
 
     def nearest(self, points: torch.Tensor) -> torch.Tensor:
         distances = torch.ones(points.shape[0], 1) * float('inf')
-        distances = distances
+        distances = distances.cuda()
+        points = points.cuda()
         for i in range(self.centers.shape[0]):
-            difference = torch.norm(self.centers[i, :] - points[:, :3], dim=1)
-            tmp_distance = difference - self.radii[i, :]
+            difference = torch.norm(self.centers[i, :].cuda() - points[:, :3], dim=1)
+            tmp_distance = difference - self.radii[i, :].cuda()
             tmp_distance = tmp_distance.reshape(points.shape[0], 1)
             distances = torch.min(distances, tmp_distance)
         return distances
@@ -34,14 +37,15 @@ class SphereGroup(ObjectGroup):
 def march(origins: torch.Tensor, directions: torch.Tensor, transform: torch.Tensor, objects: List[ObjectGroup],
           epsilon=1e-2, maximum_iterations=100, debug=False, maximum_distance=1000.0):
     transformed_rays = origins @ transform
+    transformed_rays = transformed_rays.cuda()
     rows = transformed_rays.shape[0]
-    distances = torch.ones(rows, 1) * float('inf')
+    distances = torch.ones(rows, 1).cuda() * float('inf')
     distances = distances
-    mask = torch.zeros(rows, 1).bool()
-    acc = torch.zeros_like(distances)
+    mask = torch.zeros(rows, 1).cuda().bool()
+    acc = torch.zeros_like(distances).cuda()
     if debug:
         points = transformed_rays.tolist()
-    for _ in range(maximum_iterations):
+    for _ in tqdm.tqdm(range(maximum_iterations)):
         for group in objects:
             current_distances = group.nearest(transformed_rays).reshape((rows, 1))
             distances = torch.min(distances, current_distances)
@@ -54,14 +58,13 @@ def march(origins: torch.Tensor, directions: torch.Tensor, transform: torch.Tens
                 points += transformed_rays.tolist()
         if mask.all():
             break
-        print(_)
     if debug:
         points_ = np.array(points)
         vis = open3d.geometry.PointCloud(
             points=open3d.utility.Vector3dVector(points_[:, :3])
         )
         open3d.visualization.draw_geometries([vis])
-    infinity = torch.ones(rows, 1)
+    infinity = torch.ones(rows, 1).cuda()
     infinity[~mask] = float('inf')
     infinity[acc > maximum_distance] = float('inf')
     transformed_rays += infinity
@@ -69,8 +72,8 @@ def march(origins: torch.Tensor, directions: torch.Tensor, transform: torch.Tens
 
 
 def main():
-    WIDTH = 400
-    HEIGHT = 400
+    WIDTH = 1024
+    HEIGHT = 1024
     sphere_group = SphereGroup()
     sphere_group.add_sphere([0, 0, 4], 20)
     sphere_group.add_sphere([45, 0, 800], 19)
@@ -81,18 +84,18 @@ def main():
         for y in range(-WIDTH//2, HEIGHT//2)
     ])
 
-    directions = torch.zeros_like(rays)
+    directions = torch.zeros_like(rays).cuda()
     directions = directions[:, :3]
     directions[:, 2] = 1
 
-    res = march(rays, directions, torch.eye(4, 4), [sphere_group], debug=False, maximum_iterations=1000,
-                maximum_distance=100)
+    res = march(rays, directions, torch.eye(4, 4), [sphere_group], debug=False, maximum_iterations=500,
+                maximum_distance=1000)
     res[res == float('inf')] = 0.0
     res = res.reshape(WIDTH, HEIGHT, -1)
     res = torch.norm(res, dim=2) / torch.max(res)
     res *= 255
     import matplotlib.pyplot as plot
-    plot.imshow(res)
+    plot.imshow(res.cpu())
     plot.show()
 
 
